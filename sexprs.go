000db0bc1f6d4393486692b908d6fa1c34bb3442
Tomasz Klak <tomasz.klak@gmail.com>
2013-08-09 18:39:52 +0200

    go fmt

--- sexprs.go
+++ sexprs.go
@@ -1,90 +1,100 @@
 package icfp13
 
 import (
-  "github.com/eadmund/sexprs"
-  "bytes"
+	"bytes"
+	"github.com/eadmund/sexprs"
 )
+
 type Env map[string]uint64
 
 func Parse(input []byte) sexprs.Sexp {
-  input = bytes.Replace(input, []byte(" 0"), []byte(" __0"), -1)
-  input = bytes.Replace(input, []byte(" 1"), []byte(" __1"), -1)
-  s, r, e := sexprs.Parse(input)
-  if len(r) != 0 {
-    panic("rest not empty: " + string(r))
-  }
-  if e != nil {
-    panic("failed to parse")
-  }
-  return s
+	input = bytes.Replace(input, []byte(" 0"), []byte(" __0"), -1)
+	input = bytes.Replace(input, []byte(" 1"), []byte(" __1"), -1)
+	s, r, e := sexprs.Parse(input)
+	if len(r) != 0 {
+		panic("rest not empty: " + string(r))
+	}
+	if e != nil {
+		panic("failed to parse")
+	}
+	return s
 }
 
 func Eval(e sexprs.Sexp, input Env) uint64 {
-  switch e := e.(type) {
-    case sexprs.List:
-      return evalList(e, input)
-    case sexprs.Atom:
-      return evalAtom(e, input)
-  }
-  return 4
+	switch e := e.(type) {
+	case sexprs.List:
+		return evalList(e, input)
+	case sexprs.Atom:
+		return evalAtom(e, input)
+	}
+	return 4
 }
 
 func evalList(l sexprs.List, input Env) uint64 {
-  head := string(l[0].(sexprs.Atom).Value)
-  switch head {
-    case "or": return evalOr(l[1],l[2],input)
-    case "and": return evalAnd(l[1], l[2], input)
-    case "xor": return evalXor(l[1], l[2], input)
-    case "plus": return evalPlus(l[1], l[2], input)
-    case "not": return evalNot(l[1], input)
-    case "if0": return evalIf0(l[1], l[2], l[3], input)
-    default:
-      return 52
-  }
+	head := string(l[0].(sexprs.Atom).Value)
+	switch head {
+	case "or":
+		return evalOr(l[1], l[2], input)
+	case "and":
+		return evalAnd(l[1], l[2], input)
+	case "xor":
+		return evalXor(l[1], l[2], input)
+	case "plus":
+		return evalPlus(l[1], l[2], input)
+	case "not":
+		return evalNot(l[1], input)
+	case "if0":
+		return evalIf0(l[1], l[2], l[3], input)
+	default:
+		return 52
+	}
 }
 
 func evalOr(e1, e2 sexprs.Sexp, input Env) uint64 {
-  e1v := Eval(e1, input)
-  e2v := Eval(e2, input)
-  return e1v | e2v
+	e1v := Eval(e1, input)
+	e2v := Eval(e2, input)
+	return e1v | e2v
 }
 
 func evalAnd(e1, e2 sexprs.Sexp, input Env) uint64 {
-  e1v := Eval(e1, input)
-  e2v := Eval(e2, input)
-  return e1v & e2v
+	e1v := Eval(e1, input)
+	e2v := Eval(e2, input)
+	return e1v & e2v
 }
 
 func evalXor(e1, e2 sexprs.Sexp, input Env) uint64 {
-  e1v := Eval(e1, input)
-  e2v := Eval(e2, input)
-  return e1v ^ e2v
+	e1v := Eval(e1, input)
+	e2v := Eval(e2, input)
+	return e1v ^ e2v
 }
 
 func evalPlus(e1, e2 sexprs.Sexp, input Env) uint64 {
-  e1v := Eval(e1, input)
-  e2v := Eval(e2, input)
-  return e1v + e2v
+	e1v := Eval(e1, input)
+	e2v := Eval(e2, input)
+	return e1v + e2v
 }
 
 func evalNot(e sexprs.Sexp, input Env) uint64 {
-  return ^Eval(e, input)
+	return ^Eval(e, input)
 }
 
 func evalIf0(p, zero, nonZero sexprs.Sexp, input Env) uint64 {
-  pv := Eval(p, input)
-  if pv == 0 {
-    return Eval(zero, input)
-  } else {
-    return Eval(nonZero, input)
-  }
+	pv := Eval(p, input)
+	if pv == 0 {
+		return Eval(zero, input)
+	} else {
+		return Eval(nonZero, input)
+	}
 }
 
 func evalAtom(e sexprs.Atom, input Env) uint64 {
-  es := string(e.Value)
-  switch es {
-    case "__0": return 0
-    case "__1": return 1
-    default: return input[es]
-  }
+	es := string(e.Value)
+	switch es {
+	case "__0":
+		return 0
+	case "__1":
+		return 1
+	default:
+		return input[es]
+	}
 }
--- sexprs_test.go
+++ sexprs_test.go
@@ -3,82 +3,82 @@ package icfp13
 import "testing"
 
 func TestEvalOp2(t *testing.T) {
-  data := []struct {
-    input string
-    output uint64
-  } {
-    { "(or 0 1)", 1 },
-    { "(or 1 0)", 1 },
-    { "(or 0 0)", 0 },
-    { "(or 1 1)", 1 },
-    { "(or (or (or 1 1) 1) 1)", 1 },
-    { "(and 0 0)", 0},
-    { "(and 0 1)", 0},
-    { "(and 1 0)", 0},
-    { "(and 1 1)", 1},
-    { "(xor 0 0)", 0},
-    { "(xor 0 1)", 1},
-    { "(xor 1 0)", 1},
-    { "(xor 1 1)", 0},
-    { "(plus 1 (plus 1 1))", 3},
-  }
-  for _,d := range data {
-    s := Parse([]byte(d.input))
-    if r := Eval(s, make(Env)); r != d.output {
-      t.Errorf("expected %v, got '%v'", d.output, r)
-    }
-  }
+	data := []struct {
+		input  string
+		output uint64
+	}{
+		{"(or 0 1)", 1},
+		{"(or 1 0)", 1},
+		{"(or 0 0)", 0},
+		{"(or 1 1)", 1},
+		{"(or (or (or 1 1) 1) 1)", 1},
+		{"(and 0 0)", 0},
+		{"(and 0 1)", 0},
+		{"(and 1 0)", 0},
+		{"(and 1 1)", 1},
+		{"(xor 0 0)", 0},
+		{"(xor 0 1)", 1},
+		{"(xor 1 0)", 1},
+		{"(xor 1 1)", 0},
+		{"(plus 1 (plus 1 1))", 3},
+	}
+	for _, d := range data {
+		s := Parse([]byte(d.input))
+		if r := Eval(s, make(Env)); r != d.output {
+			t.Errorf("expected %v, got '%v'", d.output, r)
+		}
+	}
 }
 
 func TestEvalId(t *testing.T) {
-  data := []struct {
-    expr string
-    env  Env
-    out  uint64
-  } {
-    { "(plus x y)", Env{"x":55,"y":45}, 100},
-  }
+	data := []struct {
+		expr string
+		env  Env
+		out  uint64
+	}{
+		{"(plus x y)", Env{"x": 55, "y": 45}, 100},
+	}
 
-  for _, d := range data {
-    s := Parse([]byte(d.expr))
-    if r := Eval(s, d.env); r != d.out {
-      t.Errorf("expected %v, got '%v'", d.out, r)
-    }
-  }
+	for _, d := range data {
+		s := Parse([]byte(d.expr))
+		if r := Eval(s, d.env); r != d.out {
+			t.Errorf("expected %v, got '%v'", d.out, r)
+		}
+	}
 }
 
 func TestEvalOp1(t *testing.T) {
-  data := []struct {
-    in string
-    out uint64
-  } {
-    { "(not 0)", 0xffffffffffffffff },
-    { "(not (not 0))", 0 },
-    { "(not (not 1))", 1 },
-  }
-  for _, d := range data {
-    s := Parse([]byte(d.in))
-    if r := Eval(s, make(Env)); r != d.out {
-      t.Errorf("expected %v, got '%v'", d.out, r)
-    }
-  }
+	data := []struct {
+		in  string
+		out uint64
+	}{
+		{"(not 0)", 0xffffffffffffffff},
+		{"(not (not 0))", 0},
+		{"(not (not 1))", 1},
+	}
+	for _, d := range data {
+		s := Parse([]byte(d.in))
+		if r := Eval(s, make(Env)); r != d.out {
+			t.Errorf("expected %v, got '%v'", d.out, r)
+		}
+	}
 }
 
 func TestEvalIf0(t *testing.T) {
-  data := []struct {
-    in string
-    env Env
-    out uint64
-  } {
-    { "(if0 0 0 1)", make(Env), 0 },
-    { "(if0 1 0 1)", make(Env), 1 },
-    { "(if0 x y z)", Env{"x":0,"y":100,"z":200}, 100},
-  }
+	data := []struct {
+		in  string
+		env Env
+		out uint64
+	}{
+		{"(if0 0 0 1)", make(Env), 0},
+		{"(if0 1 0 1)", make(Env), 1},
+		{"(if0 x y z)", Env{"x": 0, "y": 100, "z": 200}, 100},
+	}
 
-  for _, d := range data {
-    s := Parse([]byte(d.in))
-    if r := Eval(s, d.env); r != d.out {
-      t.Errorf("expected %v, got '%v'", d.out, r)
-    }
-  }
+	for _, d := range data {
+		s := Parse([]byte(d.in))
+		if r := Eval(s, d.env); r != d.out {
+			t.Errorf("expected %v, got '%v'", d.out, r)
+		}
+	}
 }
