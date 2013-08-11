28e6a8ab9137bc0fde636393e8ce583b6571d325
Tomasz Klak <tomasz.klak@gmail.com>
2013-08-11 12:22:54 +0200

    small improvement in eval

--- sexprs.go
+++ sexprs.go
@@ -7,6 +7,24 @@ import (
 
 type Env map[string]uint64
 
+var listFuns map[string]func(sexprs.List,Env) uint64
+
+func init() {
+  listFuns = make(map[string]func(sexprs.List,Env) uint64)
+  listFuns["or"] = evalOr
+  listFuns["and"] = evalAnd
+  listFuns["xor"] = evalXor
+  listFuns["plus"] = evalPlus
+  listFuns["not"] = evalNot
+  listFuns["shl1"] = evalShl1
+  listFuns["shr1"] = evalShr1
+  listFuns["shr4"] = evalShr4
+  listFuns["shr16"] = evalShr16
+  listFuns["if0"] = evalIf0
+  listFuns["lambda"] = evalLambda
+  listFuns["fold"] = evalFold
+}
+
 func Parse(input []byte) sexprs.Sexp {
 	input = bytes.Replace(input, []byte(" 0"), []byte(" __0"), -1)
 	input = bytes.Replace(input, []byte(" 1"), []byte(" __1"), -1)
@@ -38,90 +56,94 @@ func Eval(e sexprs.Sexp, input Env) uint64 {
 
 func evalList(l sexprs.List, input Env) uint64 {
 	head := string(l[0].(sexprs.Atom).Value)
-	switch head {
+  return listFuns[head](l, input)
+	/*switch head {
 	case "or":
-		return evalOr(l[1], l[2], input)
+		// return evalOr(l[1], l[2], input)
+    return evalOr(l, input)
 	case "and":
-		return evalAnd(l[1], l[2], input)
+		// return evalAnd(l[1], l[2], input)
+    return evalAnd(l, input)
 	case "xor":
-		return evalXor(l[1], l[2], input)
+		return evalXor(l, input)
 	case "plus":
-		return evalPlus(l[1], l[2], input)
+		return evalPlus(l, input)
 	case "not":
-		return evalNot(l[1], input)
+		return evalNot(l, input)
 	case "shl1":
-		return evalShl1(l[1], input)
+		return evalShl1(l, input)
 	case "shr1":
-		return evalShr1(l[1], input)
+		return evalShr1(l, input)
 	case "shr4":
-		return evalShr4(l[1], input)
+		return evalShr4(l, input)
 	case "shr16":
-		return evalShr16(l[1], input)
+		return evalShr16(l, input)
 	case "if0":
-		return evalIf0(l[1], l[2], l[3], input)
+		return evalIf0(l, input)
 	case "lambda":
-		return evalLambda(l[1], l[2], input)
+		return evalLambda(l, input)
 	case "fold":
-		return evalFold(l[1], l[2], l[3], input)
+		return evalFold(l, input)
 	default:
 		panic("unknown list head: " + head)
-	}
+	}*/
 }
 
-func evalOr(e1, e2 sexprs.Sexp, input Env) uint64 {
-	e1v := Eval(e1, input)
-	e2v := Eval(e2, input)
+func evalOr(l sexprs.List, input Env) uint64 {
+	e1v := Eval(l[1], input)
+	e2v := Eval(l[2], input)
 	return e1v | e2v
 }
 
-func evalAnd(e1, e2 sexprs.Sexp, input Env) uint64 {
-	e1v := Eval(e1, input)
-	e2v := Eval(e2, input)
+func evalAnd(l sexprs.List, input Env) uint64 {
+	e1v := Eval(l[1], input)
+	e2v := Eval(l[2], input)
 	return e1v & e2v
 }
 
-func evalXor(e1, e2 sexprs.Sexp, input Env) uint64 {
-	e1v := Eval(e1, input)
-	e2v := Eval(e2, input)
+func evalXor(l sexprs.List, input Env) uint64 {
+	e1v := Eval(l[1], input)
+	e2v := Eval(l[2], input)
 	return e1v ^ e2v
 }
 
-func evalPlus(e1, e2 sexprs.Sexp, input Env) uint64 {
-	e1v := Eval(e1, input)
-	e2v := Eval(e2, input)
+func evalPlus(l sexprs.List, input Env) uint64 {
+	e1v := Eval(l[1], input)
+	e2v := Eval(l[2], input)
 	return e1v + e2v
 }
 
-func evalNot(e sexprs.Sexp, input Env) uint64 {
-	return ^Eval(e, input)
+func evalNot(l sexprs.List, input Env) uint64 {
+	return ^Eval(l[1], input)
 }
 
-func evalShl1(e sexprs.Sexp, input Env) uint64 {
-	return Eval(e, input) << 1
+func evalShl1(l sexprs.List, input Env) uint64 {
+	return Eval(l[1], input) << 1
 }
 
-func evalShr1(e sexprs.Sexp, input Env) uint64 {
-	return Eval(e, input) >> 1
+func evalShr1(l sexprs.List, input Env) uint64 {
+	return Eval(l[1], input) >> 1
 }
 
-func evalShr4(e sexprs.Sexp, input Env) uint64 {
-	return Eval(e, input) >> 4
+func evalShr4(l sexprs.List, input Env) uint64 {
+	return Eval(l[1], input) >> 4
 }
 
-func evalShr16(e sexprs.Sexp, input Env) uint64 {
-	return Eval(e, input) >> 16
+func evalShr16(l sexprs.List, input Env) uint64 {
+	return Eval(l[1], input) >> 16
 }
 
-func evalIf0(p, zero, nonZero sexprs.Sexp, input Env) uint64 {
-	pv := Eval(p, input)
+// func evalIf0(p, zero, nonZero sexprs.Sexp, input Env) uint64 {
+func evalIf0(l sexprs.List, input Env) uint64 {
+	pv := Eval(l[1], input)
 	if pv == 0 {
-		return Eval(zero, input)
+		return Eval(l[2], input)
 	} else {
-		return Eval(nonZero, input)
+		return Eval(l[3], input)
 	}
 }
 
-func evalLambda(params, body sexprs.Sexp, input Env) uint64 {
+func evalLambda(l sexprs.List, input Env) uint64 {
 	/* p1 and p2 should already be in input!
 	pList := params.(sexprs.List)
 	p1 := string(pList[0].(sexprs.Atom).Value)
@@ -129,24 +151,24 @@ func evalLambda(params, body sexprs.Sexp, input Env) uint64 {
 	p1v := input[p1]
 	p2v := input[p2]
 	*/
-	return Eval(body, input)
+	return Eval(l[2], input)
 }
 
-func evalFold(vec, start, lambda sexprs.Sexp, input Env) uint64 {
-	vecv := Eval(vec, input)
-	acc := Eval(start, input)
+func evalFold(l sexprs.List, input Env) uint64 {
+	vecv := Eval(l[1], input)
+	acc := Eval(l[2], input)
 	for i := 0; i <= 7; i++ {
 		left := (vecv << uint((7-i)*8)) >> uint(7*8)
 		localEnv := make(Env)
 		for k, v := range input {
 			localEnv[k] = v
 		}
-		lparams := lambda.(sexprs.List)[1].(sexprs.List)
+		lparams := l[3].(sexprs.List)[1].(sexprs.List)
 		x := string(lparams[0].(sexprs.Atom).Value)
 		y := string(lparams[1].(sexprs.Atom).Value)
 		localEnv[x] = left
 		localEnv[y] = acc
-		acc = Eval(lambda, localEnv)
+		acc = Eval(l[3], localEnv)
 	}
 	return acc
 }
