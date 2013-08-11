08f184e44b66442683ec110243452ed5bd37ae8c
Tomasz Klak <tomasz.klak@gmail.com>
2013-08-11 16:12:50 +0200

    constraint update

--- tools/submiter.go
+++ tools/submiter.go
@@ -22,6 +22,7 @@ func RandomInput(size int) []string {
 		v := v1 + v2
 		ret[i] = "0x" + strconv.FormatUint(v, 16)
 	}
+ret = append(ret, "0x80000000D652A6AA")
 ret = append(ret, "0x0161C08342C010DF")
 	ret = append(ret, "0x0")
 	ret = append(ret, "0xffffffffffffffff")
